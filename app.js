2a43af4fbeb39fa34ace9c7851c8fe2376dc0e77
Vijayant Saini <vijayant.saini123@gmail.com>
2021-10-31 17:45:36 +0000

    add countries api

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+  res.send('Hello World!');
+});
 
+
+router.get('/countries', (req, res) => {
+  var countries = [];
+  var db = global.databaseConnection;
+  db.find(function (record) {
+    if(countries.find(e => e == record.country_or_area)){
+      return true;
+    } else {
+      countries.push(record.country_or_area);
+      return true;
+    }
+  }).then(function (records) {
+    res.json({
+      data: countries
+    });
+    
+  });
+});
 module.exports = router;
--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const csvdb = require('node-csv-query').default;
 
 const app = express();
 const port = process.env.PORT;
-var databaseConnection = null;
+global.databaseConnection = null;
 
 
 // register routes
-require(__dirname + "/app.js");
+app.use(require(__dirname + "/app.js"));
 
 // start server
 csvdb(__dirname + "/dataset.csv").then(function (db) {
-  databaseConnection = db;
+  global.databaseConnection = db;
   app.listen(port, (err) => {
     if(err){
       return console.log(err);
