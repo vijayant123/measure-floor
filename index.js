db1e19e9d17c6bdeb0cc98030929cacf18215239
Vijayant Saini <vijayant.saini123@gmail.com>
2021-10-31 17:17:41 +0000

    bugfix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const csvdb = require('node-csv-query').default;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 var databaseConnection = null;
 
 
